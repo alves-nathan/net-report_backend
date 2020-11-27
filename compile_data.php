b0549abb9a4048b8268819e52a9c24edd1d34281
Nathan <nathan.alves@live.com>
2020-11-27 18:40:30 -0300

    script para consolidação dos dados

--- compile_data.php
+++ compile_data.php
@@ -1,22 +1,37 @@
 <?php
 $path = "/home/pi/internet-report/data/";
-
+$return = "";
+$today = date('d-m-Y');
 if ($handle = opendir($path)) {
     while (false !== ($file = readdir($handle))) {
         if ('.' === $file) continue;
         if ('..' === $file) continue;
-
-        $content = file_get_contents($path.$file);
-        $exploded_content = explode(PHP_EOL, $content);
-        $date = explode('_', $exploded_content[0]);
+        $content = explode(PHP_EOL, file_get_contents($path.$file));
+        $date = explode('_', $content[0]);
         $return = "{";
-        array_splice($exploded_content, 0, 1);
-        foreach($exploded_content as $line){
-            var_dump(explode(" ", $line));
-            //$return .= (!empty($line))?
+        array_splice($content, 0, 1);
+        foreach($content as $line){
+            $return .= "report:{";
+            $return .= "datetime:'".$date[1].'/'.$date[0].'/'.$date[2].' '.$date[3].':'.$date[4]."',";
+            $return .= "values:{"
+            $exploded_line = explode(" ", $line);
+            $type = "";
+            $value = "";
+            foreach ($exploded_line as $word){
+                if(!empty($word)){
+                    if(strpos($word, ".")){
+                        $value = $word;
+                    } else {
+                        $type = strtolower($word);
+                    }
+                }
+                $return .= $type.':'.$value;
+            }
+            $return .= "}";
         }
         $return .= "}";
-        //var_dump($date);
+        //unset($path.$file);
     }
+    file_put_contents("/home/pi/internet-report/data_json/report_".$today.".json");
     closedir($handle);
 }
