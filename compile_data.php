a561f5ec9edee4bdc291e2f9bce402ee237d9b1a
Nathan <nathan.alves@live.com>
2020-11-27 19:26:55 -0300

    script para consolidação dos dados

--- compile_data.php
+++ compile_data.php
@@ -12,40 +12,42 @@ if ($handle = opendir($path)) {
         array_splice($content, 0, 1);
         echo("<<<<<<<<<<<<<<<<<INSIDE WHILE>>>>>>>>>>>>>>>>>>".PHP_EOL);
         var_dump($content);
+        $return .= "report:{";
+        $return .= "datetime:'".$date[1].'/'.$date[0].'/'.$date[2].' '.$date[3].':'.$date[4]."',";
         foreach($content as $line){
-            echo("<<<<<<<<<<<<<<<<<INSIDE FOREACH 1>>>>>>>>>>>>>>>>>>".PHP_EOL);
-            $return .= "report:{";
-            $return .= "datetime:'".$date[1].'/'.$date[0].'/'.$date[2].' '.$date[3].':'.$date[4]."',";
-            $json_line = "values:{";
-            $exploded_line = explode(" ", $line);
-            $type = "";
-            $value = "";
-            var_dump($exploded_line);
-            foreach ($exploded_line as $word){
-                echo("<<<<<<<<<<<<<<<<<INSIDE FOREACH 2>>>>>>>>>>>>>>>>>>".PHP_EOL);
-                var_dump($word);
-                if(!empty($word)){
-                    echo("!empty(word)".PHP_EOL);
-                    if(strpos($word, ".")){
-                        echo("strpos(word, ".")".PHP_EOL);
-                        $value = $word;
-                    } else if ($word == 'Download' || $word == 'Upload') {
-                        echo("word == 'Download' || word == 'Upload'".PHP_EOL);
-                        $type = strtolower($word);
+            if(!empty($line)){
+                $json_line = "values:{";
+                echo("<<<<<<<<<<<<<<<<<INSIDE FOREACH 1>>>>>>>>>>>>>>>>>>".PHP_EOL);
+                $exploded_line = explode(" ", $line);
+                $type = "";
+                $value = "";
+                var_dump($exploded_line);
+                foreach ($exploded_line as $word){
+                    echo("<<<<<<<<<<<<<<<<<INSIDE FOREACH 2>>>>>>>>>>>>>>>>>>".PHP_EOL);
+                    var_dump($word);
+                    if(!empty($word)){
+                        echo("!empty(word)".PHP_EOL);
+                        if(strpos($word, ".")){
+                            echo("strpos(word, ".")".PHP_EOL);
+                            $value = $word;
+                        } else if ($word == 'Download' || $word == 'Upload') {
+                            echo("word == 'Download' || word == 'Upload'".PHP_EOL);
+                            $type = strtolower($word);
+                        }
+                        var_dump($type);
+                        var_dump($value);
                     }
-                    var_dump($type);
-                    var_dump($value);
+                    if(!empty($type) && !empty($value)){
+                        $json_line .= $type.':'.$value . ",";
+                    }
                 }
-                if(!empty($type) && !empty($value)){
-                    $json_line .= $type.':'.$value . ",";
-                }
+                $json_line .= "}";
+                var_dump($json_line);
+                $return .= $json_line;
             }
-            $json_line .= "}";
-            var_dump($json_line);
-            $return .= $json_line;
+            $return .= "}";
+            //unset($path.$file);
         }
-        $return .= "}";
-        //unset($path.$file);
     }
     file_put_contents("/home/pi/internet-report/data_json/report_".$today.".json", $return);
     closedir($handle);
