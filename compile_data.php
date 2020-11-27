67800844f0cd49f9ced0834ca2b4fc2f0ca1dd16
Nathan <nathan.alves@live.com>
2020-11-27 17:25:56 -0300

    script para consolidação dos dados

--- compile_data.php
+++ compile_data.php
@@ -1,12 +1,12 @@
 <?php
-$path = "/home/pi/internet-report/data";
+$path = "/home/pi/internet-report/data/";
 
 if ($handle = opendir($path)) {
     while (false !== ($file = readdir($handle))) {
         if ('.' === $file) continue;
         if ('..' === $file) continue;
 
-        $content = file_get_contents($file);
+        $content = file_get_contents($path.$file);
 
         var_dump($content);
     }
