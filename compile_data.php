7f0c47bb52334019b59016130c45fb6941b3a98e
Nathan <nathan.alves@live.com>
2020-11-27 23:03:19 -0300

    script para consolidação dos dados

--- compile_data.php
+++ compile_data.php
@@ -10,23 +10,16 @@ if ($handle = opendir($path)) {
         $date = explode('_', $content[0]);
         $return = "{";
         array_splice($content, 0, 1);
-        echo("<<<<<<<<<<<<<<<<<INSIDE WHILE>>>>>>>>>>>>>>>>>>".PHP_EOL);
-        var_dump($content);
         $return .= "report:{";
         $return .= "datetime:'".$date[1].'/'.$date[0].'/'.$date[2].' '.$date[3].':'.$date[4]."',";
         $json_line = "values:{";
         foreach($content as $line){
             $line = trim($line);
-            var_dump($line);
-            echo("<><><><><><><><><><><><><><><><><><><><><><><>".PHP_EOL);
             if(!empty($line) && !(strpos($line, "Operadora"))){
-                echo("<<<<<<<<<<<<<<<<<INSIDE FOREACH 1>>>>>>>>>>>>>>>>>>".PHP_EOL);
                 $exploded_line = explode(" ", $line);
                 $type = "";
                 $value = "";
-                var_dump($exploded_line);
                 foreach ($exploded_line as $word){
-                    var_dump($word);
                     if(!empty($word)){
                         echo("!empty(word)".PHP_EOL);
                         if(strpos($word, ".")){
@@ -36,8 +29,6 @@ if ($handle = opendir($path)) {
                             echo("word == 'Download' || word == 'Upload'".PHP_EOL);
                             $type = strtolower($word);
                         }
-                        var_dump($type);
-                        var_dump($value);
                     }
                 }
             } elseif(strpos($line, "Operadora")){
@@ -45,6 +36,8 @@ if ($handle = opendir($path)) {
                 $value = "";
                 $type = "";
             }
+            var_dump($type);
+            var_dump($value);
             $json_line .= $type.':'.$value . ",";
             var_dump($json_line);
         }
